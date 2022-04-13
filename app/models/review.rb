b2f9de09bc4953b421c5e0ea5a61487bb22d8eab
Wayne Smart <smudgeness@gmail.com>
2022-04-13 14:12:32 +0200

    I DID IT

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: 0..5, numericality: { only_integer: true }
+end
+
+# validates :number,
+# numericality: { only_integer: true, }
